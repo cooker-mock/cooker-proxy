c103258c8624ed3a24893f0bd61e1da4226c2406
boyuan <531237512@outlook.com>
2025-01-29 18:29:50 -0500

    chore: code clean

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'popup'), // build popup only
   base: './',
   plugins: [react()],
-  server: {
-    port: process.env.PORT,
-  },
   build: {
     outDir: path.resolve(__dirname, 'popup/dist'),
     emptyOutDir: true,
